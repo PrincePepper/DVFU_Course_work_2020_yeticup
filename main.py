15c75db68f6b655e1cd726a7278528ea41ad0eb8
AlexPishchikov <cocacolaspriteman@gmail.com>
2020-12-01 11:59:13 +1000

    добавлено диалоговое окно удаления

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5 import QtWidgets
+import players
+
+class ExampleApp(QtWidgets.QMainWindow, players.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = ExampleApp()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
